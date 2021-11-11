19f6af2f52c1174fc52fb9a4afa9d3181032da25
yitzy32 <yitzybratt32@gmail.com>
2021-11-11 10:05:09 -0500

    render users data

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, {
+  useState,
+  useEffect
+} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function GitHubUser({login}) {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${login}`)
+    .then(res => res.json())
+    .then(setData)
+    .catch(console.error);
+  }, []);
+
+  if (data) {
+    console.log(data.created_at)
+    return (
+      <div>
+        <h1>{data.login}</h1>
+        <img src={data.avatar_url} width={100}/>
+        <a href={data.html_url}>View on Github</a>
+        <p>{data.location}</p>
+        <p>{data.public_repos} public repos</p>
+        <p>Github user since: {data.created_at}</p>
+      </div>
+    )
+  }
+  return null;
+}
+
 function App() {
-  return <h1>Hello</h1>
+  return <GitHubUser login="yitzy32"/>;
 }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
